ea0ebc9ce291cf49735f908be071f6ac801ffd00
Wouter Selis <113997013+Wouter-Sls@users.noreply.github.com>
2023-10-13 23:45:12 +0200

    Add files via upload

--- Task2-ML-streamlit.py
+++ Task2-ML-streamlit.py
@@ -35,19 +35,33 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-#User input
-month = st.text_input("Enter a month as a number (Jan=1, Feb=2, ...): ")
-day = st.text_input("Enter a day as a number (Monday=1, Tuesday=2, ...): ")
-x = st.text_input("Enter an X coördinate (1 to 9): ")
-y = st.text_input("Enter an Y coördinate (1 to 9): ")
-ffmc = st.text_input("Enter a FFMC index from the FWI system (0.0 to 101.0): ")
-dmc = st.text_input("Enter a DMC index from the FWI system (0.0 to 200.0): ")
-dc = st.text_input("Enter a DC index from the FWI system (0.0 to 800.0): ")
-isi = st.text_input("Enter a ISI index from the FWI system (0.0 to 60.0): ")
-temp = st.text_input("Enter a temperature in Celsius degrees: 0.0 to 45.0")
-rh = st.text_input("Enter a RH index from the FWI system (0.0 to 100.0): ")
-wind = st.text_input("Enter the wind speed in km/h: ")
-rain = st.text_input("Enter the outside rain in mm/m2: ")
+month=''
+day=''
+x=''
+y=''
+ffmc=''
+dmc=''
+dc=''
+isi=''
+temp=''
+rh=''
+wind=''
+rain=''
+
+if month == '' or day =='' or x==''or y ==''or ffmc ==''or dmc ==''or dc ==''or isi ==''or temp ==''or rh ==''or wind ==''or rain=='':
+    #User input
+    month = st.text_input("Enter a month as a number (Jan=1, Feb=2, ...): ")
+    day = st.text_input("Enter a day as a number (Monday=1, Tuesday=2, ...): ")
+    x = st.text_input("Enter an X coördinate (1 to 9): ")
+    y = st.text_input("Enter an Y coördinate (1 to 9): ")
+    ffmc = st.text_input("Enter a FFMC index from the FWI system (0.0 to 101.0): ")
+    dmc = st.text_input("Enter a DMC index from the FWI system (0.0 to 200.0): ")
+    dc = st.text_input("Enter a DC index from the FWI system (0.0 to 800.0): ")
+    isi = st.text_input("Enter a ISI index from the FWI system (0.0 to 60.0): ")
+    temp = st.text_input("Enter a temperature in Celsius degrees: 0.0 to 45.0")
+    rh = st.text_input("Enter a RH index from the FWI system (0.0 to 100.0): ")
+    wind = st.text_input("Enter the wind speed in km/h: ")
+    rain = st.text_input("Enter the outside rain in mm/m2: ")
 
 
 user_inputs={'month': month,'day':day , 'X': x, 'Y': y,'FFMC': ffmc, 'DMC': dmc, 'DC': dc, 'ISI': isi, 'temp': temp, 'RH': rh, 'wind': wind, 'rain': rain}
